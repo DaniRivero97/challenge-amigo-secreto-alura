76d5c4cb5967e964d264e485120a53b789055dc3
DaniRivero97 <daniela.riveromiranda@gmail.com>
2025-02-08 18:57:03 -0400

    inicio del proyecto

---
+++ app.js
@@ -0,0 +1,8 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombre=[];
+
+function agregarAmigo()
+{
+    let nombre = document.getElementById("amigo").value; // Obtiene el valor del input
+    console.log(nombre);
+}
