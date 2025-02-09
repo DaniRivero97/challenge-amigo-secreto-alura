fe335fc92c8ea5622d123657cc58786e73127222
DaniRivero97 <daniela.riveromiranda@gmail.com>
2025-02-08 20:34:06 -0400

    funcion-agregar-amigo

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaNombre=[];
+// alt=shit+f dar formato
+let listaNombre = [];
+let cont = 0;
+let listaAmigos = document.getElementById('listaAmigos')// referencia a un elemento en el dom
 
-function agregarAmigo()
-{
+function agregarAmigo() {
     let nombre = document.getElementById("amigo").value; // Obtiene el valor del input
     console.log(nombre);
+    //validar
+    if (nombre == "")// todos los string simpre deben estar en comilla
+    {
+        alert("Ingrese un nombre valido");
+    }
+    else {
+        listaNombre[cont] = nombre;
+        cont++;
+        // mostrar el nombre el el dom dentro de la listaAmigos
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        listaAmigos.append(li);
+    }
 }
